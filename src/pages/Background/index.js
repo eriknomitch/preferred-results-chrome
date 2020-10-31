719c46a503453a908244b984286658cff6ad3b2e
Erik Nomitch <enomitch@gmail.com>
2020-10-31 16:40:07 -0500

    Trying various permissions; executeScript

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -3,3 +3,27 @@ import '../../assets/img/icon-128.png';
 
 console.log('This is the background page.');
 console.log('Put the background scripts here.');
+
+chrome.tabs.onUpdated.addListener(function listener(tabId, changedProps, tab) {
+  // console.log(tabId);
+  // console.log(changedProps);
+  // console.log(chrome.tabs);
+  console.log("tab:");
+  console.log(tab);
+
+  chrome.tabs.executeScript(
+    tab.id,
+    {
+      code:
+        'document.getElementsByTagName("header")[0].style.backgroundColor = "red"',
+    },
+    (result) => {
+      console.log("result:");
+      console.log(result);
+    }
+  );
+
+  chrome.tabs.query({}, function (tabs) {
+    console.log(tabs);
+  });
+});
